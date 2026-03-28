4aedda84736423047855f8f1aa55dde3c5291bda
indenkun <kobamao.jp@gmail.com>
2026-03-28 13:12:45 +0900

    使っていない引数でエラーが出る問題の解決

--- build.R
+++ build.R
@@ -1,3 +1,3 @@
-if(!requireNamespace("shinylive",quiet=TRUE)) stop("shinylive required")
+if(!requireNamespace("shinylive")) stop("shinylive required")
 ver <- shinylive::assets_version()
 shinylive::export(appdir="app",destdir="docs",assets_version=ver,template_params=list(title="R言語 × 日本語記事のキュレーション"))
